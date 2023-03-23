b056b5d7799a71d1a767864df2e64f5836726bd0
sasha <sashatv126@gmail.com>
2023-03-23 13:33:13 +0300

    update

---
+++ Table/TableView/MyPorject/Cell/CellForTask2ViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CellForTask2ViewModel.swift
+//  MyPorject
+//
+//  Created by Владимир on 09.02.2022.
+//
+
+import UIKit
+
+struct CellForTask2ViewModel {
+    let nameOfSection : Date
+    let cell : [CellNewsWithDate]
+}
